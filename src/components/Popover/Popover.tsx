7c8f14ab3e0374c7b29f381bd561f7335f83db7f
HTLA380 <htetlin099798@gmail.com>
2024-10-05 23:42:20 +0630

    chore: add popover unstyled option

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -4,16 +4,26 @@ import * as React from "react";
 import * as RadixPopover from "@radix-ui/react-popover";
 
 import { buttonStyles } from "@/components/Button";
+import { UnstyledProvider, useUnstyled } from "@/context/UnstyledProvider";
 import { UnstyledProps } from "@/types";
-import { applyUnstyled, cn } from "@/utils";
-
-const Popover = RadixPopover.Root;
+import { applyUnstyled } from "@/utils";
 
 const PopoverArrow = RadixPopover.Arrow;
 
 const PopoverAnchor = RadixPopover.Anchor;
 
 const PopoverClose = RadixPopover.Close;
+
+type PopoverProps = React.ComponentProps<typeof RadixPopover.Root> &
+  UnstyledProps;
+
+const Popover = ({ unstyled = false, ...props }: PopoverProps) => {
+  return (
+    <UnstyledProvider unstyled={unstyled}>
+      <RadixPopover.Root {...props} />
+    </UnstyledProvider>
+  );
+};
 
 /* ----------------------------- PopoverTrigger ----------------------------- */
 
@@ -26,10 +36,13 @@ const PopoverTrigger = React.forwardRef<
   React.ElementRef<typeof RadixPopover.Trigger>,
   PopoverTriggerProps
 >(({ unstyled, className, ...props }) => {
+  const { unstyled: contextUnstyled } = useUnstyled();
+  const isUnstyled = unstyled ?? contextUnstyled;
+
   return (
     <RadixPopover.Trigger
       className={applyUnstyled(
-        unstyled,
+        isUnstyled,
         buttonStyles({ variant: "surface" }),
         className,
       )}
@@ -44,26 +57,39 @@ PopoverTrigger.displayName = "PopoverTrigger";
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof RadixPopover.Content>,
-  React.ComponentPropsWithoutRef<typeof RadixPopover.Content>
+  React.ComponentPropsWithoutRef<typeof RadixPopover.Content> & UnstyledProps
 >(
   (
-    { className, align = "center", side = "bottom", sideOffset = 4, ...props },
+    {
+      unstyled,
+      className,
+      align = "center",
+      side = "bottom",
+      sideOffset = 4,
+      ...props
+    },
     ref,
-  ) => (
-    <RadixPopover.Portal>
-      <RadixPopover.Content
-        ref={ref}
-        side={side}
-        align={align}
-        sideOffset={sideOffset}
-        className={cn(
-          "z-50 w-full rounded-lg border border-main-border bg-surface p-4 text-surface-text shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-bottom-6 data-[side=left]:slide-in-from-left-6 data-[side=right]:slide-in-from-right-6 data-[side=top]:slide-in-from-top-6",
-          className,
-        )}
-        {...props}
-      />
-    </RadixPopover.Portal>
-  ),
+  ) => {
+    const { unstyled: contextUnstyled } = useUnstyled();
+    const isUnstyled = unstyled ?? contextUnstyled;
+
+    return (
+      <RadixPopover.Portal>
+        <RadixPopover.Content
+          ref={ref}
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          className={applyUnstyled(
+            isUnstyled,
+            "z-50 w-full rounded-lg border border-main-border bg-surface p-4 text-surface-text shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-bottom-6 data-[side=left]:slide-in-from-left-6 data-[side=right]:slide-in-from-right-6 data-[side=top]:slide-in-from-top-6",
+            className,
+          )}
+          {...props}
+        />
+      </RadixPopover.Portal>
+    );
+  },
 );
 PopoverContent.displayName = RadixPopover.Content.displayName;
 
